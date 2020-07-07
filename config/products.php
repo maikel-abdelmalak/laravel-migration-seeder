a457db271dc075176ac06862c868360a4cc9dbca
maikel abdelmalak <maikel22@hotmail.it>
2020-07-07 20:09:07 +0200

    .

---
+++ config/products.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    [
+        'name' => 'xbox',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        'code' => '1234567895'
+    ],
+    [
+        'name' => 'pc',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        'code' => '1234587895'
+    ],
+    [
+        'name' => 'airpods',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        'code' => '4564567895'
+    ],
+]
+
+
+
+ ?>
